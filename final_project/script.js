71febb30a52fa1654bf1d4f6da54b800b2a7e10e
ritulraj8 <157919885+ritulraj8@users.noreply.github.com>
2024-09-01 01:49:27 +0530

    Add files via upload

---
+++ final_project/script.js
@@ -0,0 +1,11 @@
+document.getElementById('instagram-icon').addEventListener('click', function() {
+    window.location.href = 'https://www.instagram.com';
+  });
+
+  document.getElementById('facebook-icon').addEventListener('click', function() {
+    window.location.href = 'https://www.facebook.com/profile.php?id=61564964798940';
+  });
+
+  document.getElementById('whatsapp-icon').addEventListener('click', function() {
+    window.location.href = 'shefights02@gmail.com';
+  });
